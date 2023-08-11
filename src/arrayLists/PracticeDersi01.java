698698bb05ddd940e24f59497eae1954100146e0
tugcebol <boltugce8@gmail.com>
2023-08-11 14:17:16 +0300

    addition_11/08

---
+++ src/arrayLists/PracticeDersi01.java
@@ -0,0 +1,28 @@
+package arrayLists;
+
+import java.util.ArrayList;
+
+public class PracticeDersi01 {
+
+    // Bir ArrayList oluşturun ve içine 5 adet isim ekleyin.
+    // Daha sonra bu isimleri sondan başlayarak ekrana yazdırın.
+
+    // Örnek çıktı:
+    // Son isim: Ahmet
+    // Son ikinci isim: Mehmet
+    // ...
+
+    public static void main(String[] args) {
+
+        ArrayList<String> isimler = new ArrayList<>();
+        isimler.add("Burhan");
+        isimler.add("Resul");
+        isimler.add("Nergiz");
+        isimler.add("Ömer Faruk");
+        isimler.add("Afife");
+
+        for (int i = isimler.size() - 1; i >= 0; i--) {
+            System.out.println("Sondan " + (isimler.size() - i) + ". isim: " + isimler.get(i));
+        }
+    }
+}
